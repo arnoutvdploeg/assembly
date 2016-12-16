bfa0785405a46ebf3bd897c192e85a2a59b1c4db
arnoutvdploeg <arnout@ploegvander.nl>
2016-12-16 23:14:25 +0100

    made three versions of the hello world

---
+++ first_example/hello_uppercase.asm
@@ -0,0 +1,19 @@
+section .data
+    msg db      "Hello, world!"
+    msg2 db     10
+section .text
+    global _start
+_start:
+    mov     rax, 1
+    mov     rdi, 1
+    mov     rsi, msg
+    mov     rdx, 13
+    syscall
+    mov     rax, 1
+    mov     rdi, 1
+    mov     rsi, msg2
+    mov     rdx, 1
+    syscall 
+    mov    rax, 60
+    mov    rdi, 0
+    syscall
